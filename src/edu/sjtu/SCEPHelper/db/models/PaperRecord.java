319c491491a14271a7f64b1be582dc4e9034d590
xmxjq <xmxjq01@gmail.com>
2011-05-30 16:22:54 +0800

    最终定下了项目名称。 Signed-off-by: xmxjq <xmxjq01@gmail.com>

---
+++ src/edu/sjtu/SCEPHelper/db/models/PaperRecord.java
@@ -0,0 +1,80 @@
+package edu.sjtu.SCEPHelper.db.models;
+
+import com.j256.ormlite.dao.CloseableIterator;
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.sql.SQLClientInfoException;
+import java.sql.SQLException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gsj987
+ * Date: 11-5-22
+ * Time: 下午9:09
+ * To change this template use File | Settings | File Templates.
+ */
+
+@DatabaseTable(tableName = "paper_records")
+public class PaperRecord {
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField(canBeNull = false)
+    private int totalPoint;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private Paper paper;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private User user;
+
+    @ForeignCollectionField
+    private ForeignCollection<Answer> answers;
+
+    PaperRecord(){
+
+    }
+
+    public PaperRecord(Paper paper, User user) {
+        this.paper = paper;
+        this.user = user;
+    }
+
+    private int calcTotalPoint(){
+        CloseableIterator<Answer> iterator = answers.closeableIterator();
+        totalPoint = 0 ;
+        while(iterator.hasNext()){
+            Answer answer = iterator.next();
+            totalPoint += answer.getComment().getGainPoint();
+        }
+        try{
+            iterator.close();
+        }catch (SQLException e){
+            System.out.println(e.toString());
+        }
+        return totalPoint;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTotalPoint() {
+        return totalPoint;
+    }
+
+    public Paper getPaper() {
+        return paper;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public ForeignCollection<Answer> getAnswers() {
+        return answers;
+    }
+}
---
+++ src/edu/sjtu/SCEPHelper/net/client/BackendClient.java
@@ -0,0 +1,40 @@
+package edu.sjtu.SCEPHelper.net.client;
+
+import edu.sjtu.SCEPHelper.db.models.User;
+import edu.sjtu.SCEPHelper.net.UserResource;
+import org.restlet.resource.ClientResource;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gsj987
+ * Date: 11-5-28
+ * Time: 下午7:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class BackendClient {
+    private ClientResource resource = null;
+    private String url = null;
+    private int port;
+
+    public BackendClient(String url, int port) {
+        this.url = url;
+        this.port = port;
+
+        resource = new ClientResource(String.format("%s:%d", url, port));
+    }
+
+    public User getUser(String username){
+        resource.get();
+        return null;
+    }
+
+    public static void main(String args[]) {
+        ClientResource cr = new ClientResource("http://localhost:8182/user");
+        UserResource resource = cr.wrap(UserResource.class);
+
+        User user = resource.retrieve();
+        if (user!=null){
+            System.out.println("username:\t"+user.getUsername());
+        }
+    }
+}
